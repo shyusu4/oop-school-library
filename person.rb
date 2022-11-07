9ff7b73b9fcef8586c16967a7fb690be0ae790df
unknown <shyusupova4@gmail.com>
2022-11-07 23:23:25 +0500

    Add decorators

--- person.rb
+++ person.rb
@@ -1,8 +1,11 @@
-class Person
+require_relative './nameable'
+
+class Person < Nameable
   attr_accessor :age, :name
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..500)
     @name = name
     @age = age
@@ -15,6 +18,10 @@ class Person
     return true if @age >= 18
 
     false
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
