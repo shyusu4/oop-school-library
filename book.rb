e2bfcc2bf5758a69b5d54d837f75c2594c6ebccf
unknown <shyusupova4@gmail.com>
2022-11-09 15:09:06 +0500

    Add rentals method

--- book.rb
+++ book.rb
@@ -1,10 +1,16 @@
 require './rental'
 
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.book = self unless rental.book == self
   end
 end
--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative './nameable'
 
 class Person < Nameable
-  attr_accessor :age, :name
+  attr_accessor :age, :name, :rentals
   attr_reader :id
 
   def initialize(age, name = 'unknown', parent_permission: true)
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
