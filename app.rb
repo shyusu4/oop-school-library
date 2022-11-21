7d6ca625d0a01bed6b90297278b24f2d1315057a
melashu <meshu102@outlook.com>
2022-11-21 15:45:48 +0300

    Move entrypoint code to some other class

--- app.rb
+++ app.rb
@@ -14,51 +14,6 @@ class App
     @rentals = []
   end
 
-  def entry_point
-    puts 'Welcome to School Library App!'
-
-    loop do
-      app_options
-      app_choice = gets.chomp.to_i
-      break if app_choice == 7
-
-      options(app_choice)
-    end
-  end
-
-  def app_options
-    puts ''
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rental for a given person id'
-    puts '7 - Exit'
-  end
-
-  def options(input)
-    case input
-    when 1
-      list_books
-    when 2
-      list_people
-    when 3
-      create_person
-    when 4
-      create_book
-    when 5
-      create_rental
-    when 6
-      list_rentals
-    when 7
-      puts 'Thanks for using the school library!'
-      exit
-    else
-      puts 'Invalid Entry'
-    end
-  end
 
   def list_books
     if @books.length.zero?
@@ -144,6 +99,7 @@ class App
     @rentals << rental_item
     puts 'Rental created successfully'
   end
+  
 
   def list_rentals
     print 'ID of person: '
