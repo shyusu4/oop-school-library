f2bf44ed836c3825ba7fa0e253ae159087cbd159
melashu <meshu102@outlook.com>
2022-11-24 15:19:53 +0300

    Add fix to linter

--- spec/namebale_spec.rb
+++ spec/namebale_spec.rb
@@ -1,16 +1,16 @@
 require_relative '../nameable'
 require_relative '../person'
 describe Nameable do
- before :all do
-    person = Person.new(25, 'sherrymicronat',true)
-    @capitalizeDecorator = CapitalizeDecorator.new(person)
-    @capitalizeTrimer = TrimmerDecorator.new(@capitalizeDecorator)
- end
+  before :all do
+    person = Person.new(25, 'sherrymicronat', true)
+    @capitalize_decorator = CapitalizeDecorator.new(person)
+    @capitalize_trimer = TrimmerDecorator.new(@capitalize_decorator)
+  end
 
- it 'Check the person name is capitalized' do
-     expect(@capitalizeDecorator.correct_name).to eql 'Sherrymicronat'
- end
+  it 'Check the person name is capitalized' do
+    expect(@capitalize_decorator.correct_name).to eql 'Sherrymicronat'
+  end
   it 'Check the person name is trimmed' do
-     expect(@capitalizeTrimer.correct_name).to eql 'Sherrymic'
- end
+    expect(@capitalize_trimer.correct_name).to eql 'Sherrymic'
+  end
 end
