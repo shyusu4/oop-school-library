456d154f8e8ec2b89006c33d23813845ea5894ad
shyusu4 <shyusupova4@gmail.com>
2022-11-24 16:06:36 +0500

    Add student spec

---
+++ spec/student_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../student'
+
+describe Student do
+    before :all do
+        @student = Student.new('science', 23, 'Sofia')
+      end
+    
+      it 'checks if student class creates a new instance' do
+        expect(@student).to be_an_instance_of(Student)
+      end
+
+      it 'checks if classroom is science' do
+        expect(@student.classroom).to eql 'science'
+      end
+
+      it 'checks if age is 23' do
+        expect(@student.age).to eql 23
+      end
+    
+      it 'checks if name is Sofia' do
+        expect(@student.name).to eql 'Sofia'
+      end
+end
